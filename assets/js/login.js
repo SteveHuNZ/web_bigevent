6089accba2cc7d730b3ca9dade510ab268f99a2c
stevehu <stevehuinnz@gmail.com>
2022-05-23 17:35:34 +1200

    password verfity

--- assets/js/login.js
+++ assets/js/login.js
@@ -7,8 +7,14 @@ $(function() {
         // add events to the link of regester 
 
     $('#link_reg').on('click', function() {
-        $('.login-box').hide()
-        $('.reg-box').show()
+            $('.login-box').hide()
+            $('.reg-box').show()
+        })
+        // get form object from layui 
+    var form = layui.form
+    form.verify({
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位,且不能出现空格password should be have 6-12 characters and without sapces']
+
     })
 
 })
