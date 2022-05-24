8efcca3be962063c334934469e66d35be550c9c3
stevehu <stevehuinnz@gmail.com>
2022-05-24 22:10:45 +1200

    0524

--- assets/js/login.js
+++ assets/js/login.js
@@ -13,8 +13,31 @@ $(function() {
         // get form object from layui 
     var form = layui.form
     form.verify({
-        pwd: [/^[\S]{6,12}$/, '密码必须6到12位,且不能出现空格password should be have 6-12 characters and without sapces']
+            pwd: [/^[\S]{6,12}$/, '密码必须6到12位,且不能出现空格password should be have 6-12 characters and without sapces'],
+            // to varify two times typing should be have the same password 
+            repwd: function(value) {
+                var pwd = $('.reg-box [name=password]').val()
+                if (pwd !== value) {
+                    return 'passwords are not mathch'
+                }
+            }
 
+        })
+        // moniter the evets of form submit
+    $('#form_reg').on('submit', function(e) {
+        e.preventDefault()
+        $.post('http://ajax.frontend.itheima.net/api/reguser', {
+            username: $('#form_reg[name="username"]').val(),
+            password: $('#form_reg[name="password"]').val(),
+        }, function(res) {
+            if (res.status !== 0) {
+                return console.log(res.message);
+                console.log('register succesfully');
+            }
+
+
+        })
     })
+
 
 })
