31ef6909fb87a003f198be6cf3bfe36807bdd4bd
stevehu <stevehuinnz@gmail.com>
2022-05-27 22:57:17 +1200

    finished

--- assets/js/login.js
+++ assets/js/login.js
@@ -26,50 +26,47 @@ $(function() {
         })
         // moniter the evets of form submit
     $('#form_reg').on('submit', function(e) {
-            e.preventDefault()
-            var data = {
-                username: $('#form_reg [name=username]').val(),
-                password: $('#form_reg [name=password]').val(),
-            }
-            $.post('http://www.liulongbin.top:3007/api/reguser', data, function(res) {
-                if (res.status !== 0) {
-                    // return console.log(res.message);
-                    return layer.msg(res.message)
-
-                }
-                // console.log('register succesfully');
-                layer.msg('register succesfully')
-                    // mimic human's click action 
-                $('#link_login').click()
-
-
-            })
-        })
-        // monitering the events of form login
-        // 监听登录表单的提交事件
-    $('#form_login').submit(function(e) {
-        // 阻止默认提交行为
         e.preventDefault()
-        $.ajax({
-            url: 'http://www.liulongbin.top:3008/api/login',
-            method: 'POST',
-            // 快速获取表单中的数据
-            data: $(this).serialize(),
-            success: function(res) {
-                if (res.status !== 0) {
-                    return layer.msg('登录失败！')
-                }
-                layer.msg('登录成功！')
-                    // 将登录成功得到的 token 字符串，保存到 localStorage 中
-                localStorage.setItem('token', res.token)
-                    // 跳转到后台主页
-                location.href = '/index.html'
+        var data = {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val(),
+        }
+        $.post('http://www.liulongbin.top:3007/api/reguser', data, function(res) {
+            if (res.status !== 0) {
+                // return console.log(res.message);
+                return layer.msg(res.message)
+
             }
+            // console.log('register succesfully');
+            layer.msg('register succesfully')
+                // mimic human's click action 
+            $('#link_login').click()
+
+
         })
     })
 
+    // -------------------
+    $('#form_login').on('submit', function(e) {
+        e.preventDefault()
+        var data = {
+            username: $('#form_login [name=username]').val(),
+            password: $('#form_login [name=password]').val(),
+        }
+        $.post('http://www.liulongbin.top:3008/api/login', data, function(res) {
+            if (res.status !== 0) {
+                // return console.log(res.message);
+                return layer.msg(res.message)
+
+            }
+            // console.log('register succesfully');
+            layer.msg('register succesfully')
+                // mimic human's click action 
+            console.log(res.token);
 
 
 
+        })
+    })
 
 })
